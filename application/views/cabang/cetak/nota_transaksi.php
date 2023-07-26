c62588ba9023056f5b34c62d73baa90e9fb6dfc9
Adnan irfan rosyidi <adnanirfanrosyidi@gmail.com>
2023-07-26 20:46:21 +0700

    custom print nota

--- application/views/cabang/cetak/nota_transaksi.php
+++ application/views/cabang/cetak/nota_transaksi.php
@@ -27,7 +27,7 @@
         }
 
         body {
-            font-size: 8px !important;
+            font-size: 12px !important;
             letter-spacing: 2 !important;
         }
 
@@ -36,7 +36,7 @@
         }
 
         .font-9 {
-            font-size: 9px;
+            font-size: 12px;
         }
     </style>
 </head>
@@ -157,98 +157,7 @@
 
     <hr>
 
-    <table>
-        <tr>
-            <td style="vertical-align: top;width: 60%;">
-                <table cellspacing="0">
-                    <tr>
-                        <th>No.</th>
-                        <th>Ketentuan Garansi</th>
-                    </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>Klaim garansi wajib menyertakan bukti pembayaran berupa nota transaksi perbaikan.</td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>Pelanggan yang tidak dapat menunjukkan struk atau nota fisik maka dapat menunjukkannya melalui bentuk foto.</td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>Pelanggan yang kehilangan nota dan tidak memiliki bukti foto akan tetap dilayani proses klaimnya selama nota perbaikan dapat dilakukan tracking untuk dicetak kembali.</td>
-                    </tr>
-                    <tr>
-                        <td>4.</td>
-                        <td>Nota pelanggan yang tidak dapat dilakukan tracking maka tidak dapat dilayani proses klaim garansinya.</td>
-                    </tr>
-                    <tr>
-                        <td>5.</td>
-                        <td>Pelanggan yang mengalami kendala pada perangkatnya dapat mengonfirmasi terlebih dahulu keluhannya ke nomor Whatsapp yang tertera pada nota jika tidak dapat datang ke store di waktu tersebut.</td>
-                    </tr>
-                    <tr>
-                        <td>6.</td>
-                        <td>Garansi berlaku di seluruh cabang iFixied dan terhitung sejak tanggal pengambilan perangkat.</td>
-                    </tr>
-                    <tr>
-                        <td>7.</td>
-                        <td>Garansi tidak berlaku pada item diluar perbaikan dari nota transaksi.</td>
-                    </tr>
-                    <tr>
-                        <td>8.</td>
-                        <td>Pelanggan diperkenankan melakukan klaim garansi maksimal sebanyak 3 kali selama masa periode garansi.</td>
-                    </tr>
-                    <tr>
-                        <td>9.</td>
-                        <td>Garansi tidak berlaku apabila terjadi indikasi human error (kelalaian penggunaan) seperti terjatuh, terhempas, tertekan, kena air, dan over charging.</td>
-                    </tr>
-                    <tr>
-                        <td>10.</td>
-                        <td>Garansi tidak berlaku jika segel service rusak.</td>
-                    </tr>
-                    <tr>
-                        <td>11.</td>
-                        <td>Segala kerusakan fungsional (LCD, speaker, keyboard, dll) yang ditemukan dan data yang hilang setelah selesai perbaikan untuk unit yang masuk dalam keadaan NO CHECK (Mati total, blank, no touch, stuck logo, disable) adalah diluar tanggung jawab iFixied.</td>
-                    </tr>
-                    <tr>
-                        <td>12.</td>
-                        <td>Garansi hanya berlaku untuk barang yang diperbaiki atau diservice. Jika ada kerusakan selain hal yang diperbaiki, bukan tanggung jawab iFixied.</td>
-                    </tr>
-                </table>
-            </td>
-            <td style="vertical-align: top;">
-                <table cellspacing="0">
-                    <tr>
-                        <th>No.</th>
-                        <th>Persyaratan Khusus & Masa Garansi</th>
-                    </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>Garansi IC/Mesin (Berlaku 1 Bulan) : Garansi service mesin hanya berlaku sesuai dengan kendala awal kerusakan dan berada pada jalur ic yang sama.</td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>Garansi LCD (Berlaku 1 Tahun) : Garansi LCD/Touchscreen berlaku jika kondisi fisik LCD seperti baru, hanya pada kendala LCD blank hitam, touchscreen error, dan white spot (factor pemasangan). Garansi LCD tidak berlaku apabila terjadi kerusakan pada fisik (LCD baret atau gores, retak, pecah luar, pecah dalam, Flexible Robek, Korosi). Garansi LCD tidak berlaku apabila tampilan LCD bergaris atau black spot.</td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>Garansi Battery (Berlaku 1 Tahun) : Garansi battery tidak berlaku apabila terjadi indikasi over charging, battery menggelembung, Flexible tidak robek, tidak terbakar/korsleting.</td>
-                    </tr>
-                    <tr>
-                        <td>4.</td>
-                        <td>Garansi Kamera, Home Button, Connector, Volume (Berlaku 3 bulan) : Garansi kamera tidak berlaku apabila kamera berjamur, Flexible Robek. Home button, Connector, Volume tidak berlaku apabila flexible robek, tidak terbakar/korsleting.</td>
-                    </tr>
-                    <tr>
-                        <td>5.</td>
-                        <td>Garansi Aksesoris : Aksesoris berlaku selama 1 minggu, aksesoris tidak terkena air, tidak terjatuh, fisik seperti pertama beli (Garansi MagSafe 1 Bulan).</td>
-                    </tr>
-                    <tr>
-                        <td>6.</td>
-                        <td>Barang yang tidak diambil selama 1 bulan, hilang/rusak bukan tanggung jawab dari iFixied.</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    
 </body>
 
 </html>
